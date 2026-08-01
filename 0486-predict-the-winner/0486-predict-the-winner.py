7f5c100c7b2671cb307b6b18b50142d8136a526b
Pradnesh02 <kprady02@gmail.com>
2026-08-01 22:46:10 +0530

    Time: 1 ms (69.94%), Space: 12.8 MB (9.25%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -5,23 +5,25 @@ class Solution(object):
         :rtype: bool
         """
         n = len(nums)
-        # memoization table for subproblems (i, j)
         memo = {}
 
         def maxDiff(i, j):
+            # Base case: only one element left
             if i == j:
                 return nums[i]
             
             if (i, j) in memo:
                 return memo[(i, j)]
 
-            # Option 1: Pick nums[i] from the start
+            # Option 1: Pick the element at the start (i)
             pick_left = nums[i] - maxDiff(i + 1, j)
-            # Option 2: Pick nums[j] from the end
+            
+            # Option 2: Pick the element at the end (j)
             pick_right = nums[j] - maxDiff(i, j - 1)
 
+            # Store and return the maximum relative score possible
             memo[(i, j)] = max(pick_left, pick_right)
             return memo[(i, j)]
 
-        # Player 1 wins if the relative score difference is >= 0
+        # Player 1 wins if relative score difference is >= 0
         return maxDiff(0, n - 1) >= 0
