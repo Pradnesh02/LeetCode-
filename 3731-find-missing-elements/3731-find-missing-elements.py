59cd7e65fe722c783ee149a24d8e36a016c22f9b
Pradnesh02 <kprady02@gmail.com>
2026-08-04 22:17:36 +0530

    Time: 7 ms (52.53%), Space: 12.4 MB (51.38%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findMissingElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        num_set = set(nums)
+        min_val = min(nums)
+        max_val = max(nums)
+        
+        # Iterate sequentially through the range; this automatically returns a sorted result
+        return [x for x in range(min_val, max_val + 1) if x not in num_set]
