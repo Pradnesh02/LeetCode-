a171a28141cf05fd7d3a9b4275e373710117f270
Pradnesh02 <kprady02@gmail.com>
2026-08-11 22:27:47 +0530

    Time: 0 ms (100%), Space: 12.3 MB (90.78%) - LeetHub

---
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def sumBase(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        digit_sum = 0
+        
+        while n > 0:
+            digit_sum += n % k
+            n //= k
+            
+        return digit_sum
