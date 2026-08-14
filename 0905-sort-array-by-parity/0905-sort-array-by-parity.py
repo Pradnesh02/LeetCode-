8877ff7dae22ef179090ad3e8bde8325dfb522e7
Pradnesh02 <kprady02@gmail.com>
2026-08-14 23:49:42 +0530

    Time: 3 ms (55.45%), Space: 13 MB (33.32%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            if nums[left] % 2 > nums[right] % 2:
+                # nums[left] is odd and nums[right] is even -> swap
+                nums[left], nums[right] = nums[right], nums[left]
+
+            if nums[left] % 2 == 0:
+                left += 1
+            if nums[right] % 2 == 1:
+                right -= 1
+
+        return nums
