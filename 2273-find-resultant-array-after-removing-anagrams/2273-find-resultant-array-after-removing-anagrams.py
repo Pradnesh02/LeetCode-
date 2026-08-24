050f26cfb676a65fd06aed39855d49c9a796872c
Pradnesh02 <kprady02@gmail.com>
2026-08-24 22:16:45 +0530

    Time: 15 ms (30.14%), Space: 12.4 MB (70.81%) - LeetHub

---
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeAnagrams(self, words):
+        res = [words[0]]
+        
+        for i in range(1, len(words)):
+            if sorted(words[i]) != sorted(words[i - 1]):
+                res.append(words[i])
+                
+        return res
