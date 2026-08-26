04987659310cf9496d826d73dc51f0a5baad1e95
Pradnesh02 <kprady02@gmail.com>
2026-08-26 22:31:10 +0530

    Time: 3 ms (44.56%), Space: 19.3 MB (94.04%) - LeetHub

---
+++ 2293-min-max-game/2293-min-max-game.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def minMaxGame(self, nums: List[int]) -> int:
+        while len(nums) > 1:
+            n = len(nums)
+            new_nums = []
+            
+            for i in range(n // 2):
+                if i % 2 == 0:
+                    new_nums.append(min(nums[2 * i], nums[2 * i + 1]))
+                else:
+                    new_nums.append(max(nums[2 * i], nums[2 * i + 1]))
+                    
+            nums = new_nums
+            
+        return nums[0]
