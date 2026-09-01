0711e9e2ffa95a651c0c4ef7a91a83875f0985ba
Pradnesh02 <kprady02@gmail.com>
2026-09-01 22:32:39 +0530

    Time: 72 ms (66.27%), Space: 26.1 MB (44.23%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.py
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -1,37 +1,27 @@
-class Solution(object):
-    def stoneGameII(self, piles):
-        """
-        :type piles: List[int]
-        :rtype: int
-        """
+from functools import lru_cache
+from typing import List
+
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
         n = len(piles)
         
-        # Suffix sums array to get total remaining stones from index i in O(1) time
+        # Precompute suffix sums
         suffix_sum = [0] * (n + 1)
         for i in range(n - 1, -1, -1):
             suffix_sum[i] = suffix_sum[i + 1] + piles[i]
             
-        memo = {}
-        
-        def dp(i, M):
-            # Base case: if remaining piles can all be taken, take all of them
-            if i + 2 * M >= n:
+        @lru_cache(None)
+        def dp(i: int, m: int) -> int:
+            # If all remaining piles can be taken in one turn
+            if i + 2 * m >= n:
                 return suffix_sum[i]
             
-            if (i, M) in memo:
-                return memo[(i, M)]
-            
             max_stones = 0
-            
-            # The player can take X piles where 1 <= X <= 2 * M
-            for X in range(1, 2 * M + 1):
-                # Total remaining stones minus the opponent's best possible score from the next state
-                opponent_score = dp(i + X, max(M, X))
-                current_score = suffix_sum[i] - opponent_score
-                max_stones = max(max_stones, current_score)
+            for x in range(1, 2 * m + 1):
+                # Total remaining stones minus what the next player gets optimally
+                current_take = suffix_sum[i] - dp(i + x, max(m, x))
+                max_stones = max(max_stones, current_take)
                 
-            memo[(i, M)] = max_stones
             return max_stones
-
-        # Alice starts at index 0 with M = 1
+        
         return dp(0, 1)
